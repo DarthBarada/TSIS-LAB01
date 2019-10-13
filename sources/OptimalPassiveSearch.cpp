4e091d8d55cacb9e42412b6d84896f1fa3b0c037
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-10-13 19:26:14 +0300

    Update OptimalPassiveSearch.cpp

--- sources/OptimalPassiveSearch.cpp
+++ sources/OptimalPassiveSearch.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "../include/OptimalPassiveSearch.h"
 
 OptimalPassiveSearch::OptimalPassiveSearch(long double bottom, long double top, long double e)
@@ -14,30 +13,30 @@ double OptimalPassiveSearch::length_of_undefined_segment(int N)
 void OptimalPassiveSearch::pass()
 	{
 		double dotx = 0;
-		std::multimap< long double, std::pair<long double, long double>> function_results; //Контейнер упорядоченных значений для хранения значений функции
+		std::multimap< long double, std::pair<long double, long double>> function_results; //ГЉГ®Г­ГІГҐГ©Г­ГҐГ° ГіГЇГ®Г°ГїГ¤Г®Г·ГҐГ­Г­Г»Гµ Г§Г­Г Г·ГҐГ­ГЁГ© Г¤Г«Гї ГµГ°Г Г­ГҐГ­ГЁГї Г§Г­Г Г·ГҐГ­ГЁГ© ГґГіГ­ГЄГ¶ГЁГЁ
 
 		setlocale(LC_ALL, "RU");
-		std::cout << std::left << "Оптимальный пассивный поиск\n"
+		std::cout << std::left << "ГЋГЇГІГЁГ¬Г Г«ГјГ­Г»Г© ГЇГ Г±Г±ГЁГўГ­Г»Г© ГЇГ®ГЁГ±ГЄ\n"
 			<< "___________________________\n"
-			<< "|" << std::setw(11) << "Количество" << "|" << std::setw(11) << "Значение x" << "|\n"
-			<< "|" << std::setw(11) << "точек (N)" << "|" << std::setw(11) << "в минимуме" << "|\n"
+			<< "|" << std::setw(11) << "ГЉГ®Г«ГЁГ·ГҐГ±ГІГўГ®" << "|" << std::setw(11) << "Г‡Г­Г Г·ГҐГ­ГЁГҐ x" << "|\n"
+			<< "|" << std::setw(11) << "ГІГ®Г·ГҐГЄ (N)" << "|" << std::setw(11) << "Гў Г¬ГЁГ­ГЁГ¬ГіГ¬ГҐ" << "|\n"
 			<< "---------------------------\n";
 
 		auto start = std::chrono::high_resolution_clock::now();
 
 		for (int N = 1; length_of_undefined_segment(N) >= 2 * delta; N++)
 			{
-				function_results.clear();				// Располагаться должно здесь,чтобы при последней итерации не стерло
+				function_results.clear();				// ГђГ Г±ГЇГ®Г«Г ГЈГ ГІГјГ±Гї Г¤Г®Г«Г¦Г­Г® Г§Г¤ГҐГ±Гј,Г·ГІГ®ГЎГ» ГЇГ°ГЁ ГЇГ®Г±Г«ГҐГ¤Г­ГҐГ© ГЁГІГҐГ°Г Г¶ГЁГЁ Г­ГҐ Г±ГІГҐГ°Г«Г®
 				for (int k = 1; k <= N; k++)
 					{
 						dotx = bottom_edge + k * (top_edge - bottom_edge) / (N + 1.0);
 						function_results.insert(std::make_pair(F(dotx), std::make_pair(dotx, length_of_undefined_segment(N) / 2)));
 					}
 				auto info = function_results.begin();
-				std::cout << std::setprecision(4) << "|" << std::setw(11) << N << "|" << round(info->second.first * 1000) / 1000 << "±" << info->second.second << "\n";
+				std::cout << std::setprecision(4) << "|" << std::setw(11) << N << "|" << round(info->second.first * 1000) / 1000 << "В±" << info->second.second << "\n";
 			}
 		std::cout << "--------------------------\n";
 		auto end = std::chrono::high_resolution_clock::now();
 		timer = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-		std::cout << "Время нахождения: " << timer.count() << " ns\n";
+		std::cout << "Г‚Г°ГҐГ¬Гї Г­Г ГµГ®Г¦Г¤ГҐГ­ГЁГї: " << timer.count() << " ns\n";
 	}
