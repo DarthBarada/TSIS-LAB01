dca98042405f084abc55b1f2d0028306f822d953
MIB <48214233+DarthBarada@users.noreply.github.com>
2019-10-13 19:26:04 +0300

    Update GoldrenSection.cpp

--- sources/GoldrenSection.cpp
+++ sources/GoldrenSection.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "../include/GoldrenSection.h"
 
 GoldrenSection::GoldrenSection()
@@ -13,7 +12,7 @@ GoldrenSection::GoldrenSection(long double bottom, long double top, long double 
 		top_edge = top; bottom_edge = bottom; delta = e;
 		interval = top - bottom;
 
-		//		Подготовка 2 точек к 1 шагу
+		//		ГЏГ®Г¤ГЈГ®ГІГ®ГўГЄГ  2 ГІГ®Г·ГҐГЄ ГЄ 1 ГёГ ГЈГі
 		long double dot = 0;
 		dot = bottom_edge + (1 - 1 / t)*top_edge;
 		first_point = std::make_pair(dot, F(dot));
@@ -63,16 +62,16 @@ void GoldrenSection::update_point2()
 void GoldrenSection::pass()
 	{
 		setlocale(LC_ALL, "RU");
-		std::cout <<std::left<<"\nМетод золотого сечения\n"
+		std::cout <<std::left<<"\nГЊГҐГІГ®Г¤ Г§Г®Г«Г®ГІГ®ГЈГ® Г±ГҐГ·ГҐГ­ГЁГї\n"
 			<< "_______________________________________________________\n"
-			<< std::setw(13) << "" << "|" << std::setw(10) << "Длина" << "||" << std::setw(6) << "" << "|" << std::setw(6) << "" << "||" << std::setw(6) << "" << "|" << std::setw(6) << "" << "|\n"
-			<< std::setw(13) << "Интервал" << "|" << std::setw(10) << "интервала" << "||" << std::setw(6) << "X1" << "|" << std::setw(6) << "X2" << "||" << std::setw(6) << "F(X1)" << "|" << std::setw(6) << "F(X2)" << "|\n"
+			<< std::setw(13) << "" << "|" << std::setw(10) << "Г„Г«ГЁГ­Г " << "||" << std::setw(6) << "" << "|" << std::setw(6) << "" << "||" << std::setw(6) << "" << "|" << std::setw(6) << "" << "|\n"
+			<< std::setw(13) << "Г€Г­ГІГҐГ°ГўГ Г«" << "|" << std::setw(10) << "ГЁГ­ГІГҐГ°ГўГ Г«Г " << "||" << std::setw(6) << "X1" << "|" << std::setw(6) << "X2" << "||" << std::setw(6) << "F(X1)" << "|" << std::setw(6) << "F(X2)" << "|\n"
 			<< std::setw(13) << "_____________" << "|" << std::setw(10) << "__________" << "||" << std::setw(6) << "______" << "|" << std::setw(6) << "______" << "||" << std::setw(6) << "______" << "|" << std::setw(6) << "______" << "|\n"
 			<< std::setw(13) << ""			    << "|" << std::setw(10) << ""			<< "||" << std::setw(6) << ""		<< "|" << std::setw(6) << ""	   << "||" << std::setw(6) << ""	   << "|" << std::setw(6) << ""		  << "|\n";
 		auto start = std::chrono::high_resolution_clock::now();
 		int kol = 1;
 		std::cout << std::left <<"[" << std::setw(5) <<bottom_edge<<","<< std::setw(5)<<top_edge<<"]"<< "|" << std::setw(10) << interval << "||" << std::setw(6) << first_point.first << "|" << std::setw(6) << second_point.first << "||" << std::setw(6) << first_point.second << "|" << std::setw(6) << second_point.second << "|\n";
-		///					Первый шаг
+		///					ГЏГҐГ°ГўГ»Г© ГёГ ГЈ
 		if (first_point.second < second_point.second)
 			{
 				update_point1();
@@ -84,7 +83,7 @@ void GoldrenSection::pass()
 				update_point2();
 			}
 		std::cout << std::left << "[" << std::setw(5) << bottom_edge << "," << std::setw(5) << top_edge << "]" << "|" << std::setw(10) << interval << "||" << std::setw(6) << first_point.first << "|" << std::setw(6) << second_point.first << "||" << std::setw(6) << first_point.second << "|" << std::setw(6) << second_point.second << "|\n";
-		///					Второй шаг
+		///					Г‚ГІГ®Г°Г®Г© ГёГ ГЈ
 		while (interval >= delta)
 			{
 				if (first_point.second < second_point.second)
@@ -109,8 +108,8 @@ void GoldrenSection::pass()
 		timer = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 
 		std::cout << "_______________________________________________________\n"
-			<<"Результат: " << bottom_edge + (top_edge - bottom_edge) / 2 << "\n"
-			<< "x* принадлежит [ " << bottom_edge << " , " << top_edge << " ] " << "\n"
+			<<"ГђГҐГ§ГіГ«ГјГІГ ГІ: " << bottom_edge + (top_edge - bottom_edge) / 2 << "\n"
+			<< "x* ГЇГ°ГЁГ­Г Г¤Г«ГҐГ¦ГЁГІ [ " << bottom_edge << " , " << top_edge << " ] " << "\n"
 			<< "--------------------------\n";;
-		std::cout << "Время нахождения: " << timer.count() << " ns\n\n";
+		std::cout << "Г‚Г°ГҐГ¬Гї Г­Г ГµГ®Г¦Г¤ГҐГ­ГЁГї: " << timer.count() << " ns\n\n";
 	}
